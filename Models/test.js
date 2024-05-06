e4e9aadc1e61f810f9b0dc4a0ed1f3edb96b556f
JousefAly <youssefali00286@gmail.com>
2024-05-07 00:27:32 +0300

    read and read with filters

--- Models/test.js
+++ Models/test.js
@@ -3,14 +3,23 @@ const BlogPost = require('./BlogPost')
 
 mongoose.connect('mongodb://localhost/Blog');
 
-BlogPost.create({
-    title: "test title 2",
-    body: "test body hehehe 2"
-})
-.then((result) => {
-    console.log(result)
-})
-.catch((error) => {
-    console.log(error)
-})
+// BlogPost.create({
+//     title: "test title 2",
+//     body: "test body hehehe 2"
+// })
+// .then((result) => {
+//     console.log(result)
+// })
+// .catch((error) => {
+//     console.log(error)
+// })
+
+BlogPost.find({ body: 'test body hehehe' })
+    .then((result) => {
+        console.log(result)
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
 
